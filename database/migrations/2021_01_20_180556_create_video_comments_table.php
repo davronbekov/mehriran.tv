22cc7a70663e5dc4826d8200f32cb701646d0cc8
davronbekov <davronbekov.otabek@gmail.com>
2021-01-21 00:02:00 +0500

    Comments :: init

---
+++ database/migrations/2021_01_20_180556_create_video_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideoCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video_comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('file_id');
+            $table->integer('user_id');
+            $table->string('comment', 255);
+            $table->tinyInteger('visible')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('video_comments');
+    }
+}
--- resources/views/pages/articles/show.blade.php
+++ resources/views/pages/articles/show.blade.php
@@ -14,7 +14,29 @@
                    {!! $article->description ?? 'undefined' !!}
                </div>
            </div>
+           <div class="col-md-8 offset-md-2 mt-4">
+               <h4>Comments:</h4>
+           </div>
+           <div class="col-md-8 offset-md-2 mt-4">
+               <div class="card">
+                   <div class="card-body">
+                       <h5 class="mt-0">Media heading</h5>
+                       Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                   </div>
+               </div>
+           </div>
+
+           <div class="col-md-8 offset-md-2 mt-4">
+               <div class="card">
+                   <div class="card-body">
+                       <h5 class="mt-0">Media heading</h5>
+                       Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                   </div>
+               </div>
+           </div>
        </div>
    </div>
+
+
 
 @endsection
